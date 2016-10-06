587a47876fb817c3843669c56df0712cda94127c
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-06 10:24:11 -0400

    Enabled log display of NSURLSessionDataTask data

--- NerdFeed/AppDelegate.m
+++ NerdFeed/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BNRCoursesViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    BNRCoursesViewController *cvc = [[BNRCoursesViewController alloc] initWithStyle:UITableViewStylePlain];
+    UINavigationController *masterNav = [[UINavigationController alloc] initWithRootViewController:cvc];
+    
+    self.window.rootViewController = masterNav;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ NerdFeed/BNRCoursesViewController.h
@@ -0,0 +1,15 @@
+//
+//  BNRCoursesViewController.h
+//  NerdFeed
+//
+//  Created by Leksha Ramdenee on 2016-10-06.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRCoursesViewController : UITableViewController
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
